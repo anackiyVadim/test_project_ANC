78e947672ff5a9dcdf6b13f18d4150cfcf6e1487
38066 <@https://github.com/anackiyVadim/ItstepHw.git>
2024-06-22 11:27:17 +0300

    Second commit, creation of basic models (prototypes) ....... Второй комит, создание базовых моделей(прототипов)

--- website/hierarchyEmployees/models.py
+++ website/hierarchyEmployees/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Position(models.Model):
+    position_name = models.CharField(max_length=60, verbose_name='Найменування посади')
+
+    def __str__(self):
+        return self.position_name
+
+    class Meta:
+        verbose_name = 'Посади'
+        verbose_name_plural = 'Посади'
+
+class Employee(models.Model):
+    name = models.TextField(verbose_name=f'Ім`я')
+    surname = models.TextField(verbose_name='Призвище')
+    surname_patronymic = models.TextField(verbose_name='По батькові')
+    data_admission = models.DateField(verbose_name='Дата прийому')
+    email = models.EmailField(verbose_name='Email')
+    position = models.ForeignKey(Position, on_delete=models.CASCADE, verbose_name='Посада')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Cпівробітник'
+        verbose_name_plural = 'Співробітники'
+
