c396f8952ed53978d467be5fd3274d763f44baa7
38066 <@https://github.com/anackiyVadim/ItstepHw.git>
2024-06-22 12:14:11 +0300

    Commit #6, import into the Bootstrap project, basic setup of the main templates. ....... Комит №6, импорт в проєкт Bootstrap, базавая настройка главных шаблонов. ...... Коміт №6, імпорт у проект Bootstrap, базове налаштування головних шаблонів.

--- website/hierarchyEmployees/views.py
+++ website/hierarchyEmployees/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def base():
+    context = {}
+    return context
+
+
+def index(request):
+    context = {}
+    context.update(base())
+    return render(request, 'hierarchyEmployees/index.html', context)
